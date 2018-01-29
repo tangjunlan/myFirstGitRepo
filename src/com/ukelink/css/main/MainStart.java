486e63a709789860c34485b90087acfd165adbdc
tangjunlan <tangjunlan@ukelink.com>
2018-01-29 18:01:48 +0800

    this is test

---
+++ src/com/ukelink/css/main/MainStart.java
@@ -0,0 +1,10 @@
+package com.ukelink.css.main;
+
+public class MainStart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("************** success ***************");
+	}
+
+}
